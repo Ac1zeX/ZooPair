9b264c086c5342de86f48da5d3accea3980d6eb0
Zelenuy-drakon <maksfrolov2904@gmail.com>
2026-02-24 19:11:54 +0300

    vb

---
+++ Zoo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Zoo
+{
+    private List<Animal> _animals = new List<Animal>();
+    public void AddAnimal(Animal animal)
+    {
+        _animals.Add(animal);
+    }
+    public void ShowAll()
+    {
+        foreach (var animal in _animals)
+        {
+            animal.MakeSound();
+        }
+    }
+}
+
